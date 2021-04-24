2b85d359fc80636c7b289107ca22e926623d5b44
Aikoyori <12034280+Aikoyori@users.noreply.github.com>
2021-04-24 23:21:49 +0700

    initial error page stuff idk lol

---
+++ outdated/ie9/errorPageStrings.js
@@ -0,0 +1,73 @@
+//Split out for localization.
+var L_GOBACK_TEXT = "Go back to the previous page.";
+var L_REFRESH_TEXT = "Refresh the page.";
+var L_MOREINFO_TEXT = "More information";
+var L_OFFLINE_USERS_TEXT = "For offline users";
+var L_RELOAD_TEXT = "Retype the address.";
+var L_HIDE_HOTKEYS_TEXT = "Hide tab shortcuts";
+var L_SHOW_HOTKEYS_TEXT = "Show more tab shortcuts";
+var L_CONNECTION_OFF_TEXT = "You are not connected to the Internet. Check your Internet connection.";
+var L_CONNECTION_ON_TEXT = "It appears you are connected to the Internet, but you might want to try to reconnect to the Internet.";
+
+//used by invalidcert.js and hstscerterror.js
+var L_CertUnknownCA_TEXT = "Your PC doesn\u2019t trust this website\u2019s security certificate.";
+var L_CertExpired_TEXT = "The website\u2019s security certificate is not yet valid or has expired.";
+var L_CertCNMismatch_TEXT = "The hostname in the website\u2019s security certificate differs from the website you are trying to visit.";
+var L_CertRevoked_TEXT = "This website\u2019s security certificate has been revoked, so you can\u2019t go there at this time.";
+var L_CertSigFailed_TEXT = "The website\u2019s security certificate is not secure.";
+var L_CertWeakSignature_TEXT = "The website\u2019s security certificate has a weak signature and is not secure.";
+
+var L_PhishingThreat_TEXT = "Phishing threat: This is a phishing website that impersonates a trusted website to trick you into revealing personal or financial information.";
+var L_MalwareThreat_TEXT = "Malicious software threat: This site contains links to viruses or other software programs that can reveal personal information stored or typed on your computer to malicious persons.";
+
+var L_PUAATitle_TEXT = "Reported Website: Navigation Cancelled";
+
+var L_TopFrameUnsafe_TEXT = "This website has been reported as unsafe";
+var L_TopFrameUnwanted_TEXT = "Be careful when downloading programs from this website";
+var L_ContentUnsafe_TEXT = "Content on this website has been reported as unsafe";
+var L_Content_TEXT = "Hosted by:  ";
+
+var L_ACR_Title_TEXT = "We were unable to return you to %s.";
+var L_ACR_TitleFallback_TEXT = "We were unable to return you to the page you were viewing.";
+var L_ACR_ReturnTo_TEXT = "Try to return to %s";
+var L_ACR_ReturnToFallback_TEXT = "Try to return to the page you were viewing";
+var L_ACR_GoHome_TEXT = "Go to your home page";
+
+//used by new dnserror.htm
+var L_INTERNET_NOT_CONNECTED_TEXT = "You\u2019re not connected to a network";
+var L_INTERNET_CONNECTED_TEXT = "Can\u2019t reach this page";
+
+var L_RESOURCE_NOT_FOUND_TEXT = "This website could not be found.";
+var L_RESOURCE_NOT_FOUND_WSAECONNRESET_TEXT = "The DNS server refused the connection.";
+var L_RESOURCE_NOT_FOUND_ERROR_TIMEOUT_TEXT = "The connection to the DNS server timed out.";
+var L_RESOURCE_NOT_FOUND_ERROR_SUCCESS_TEXT = "There was a temporary DNS error. Try refreshing the page.";
+var L_RESOURCE_NOT_FOUND_RCODE_NAME_ERROR_TEXT = "The DNS name does not exist.";
+var L_RESOURCE_NOT_FOUND_ERROR_OTHER_TEXT = "The DNS server might be having problems.";
+
+var L_DOWNLOAD_FAILURE_TEXT = "The connection to the website was reset.";
+var L_REDIRECT_FAILED_TEXT = "There were too many redirections.";
+var L_CONNECTION_TIMEOUT_TEXT = "It took too long to connect to this website.";
+var L_DATA_NOT_AVAILABLE_TEXT = "We connected to the website, but didn\u2019t receive any data from it.";
+
+var L_ERROR_CODE_TEXT = "Error Code: ";
+
+var L_TLS_SSL_TEXT = "Make sure TLS and SSL protocols are enabled. Go to Tools > Internet Options > Advanced > Settings > Security";
+
+// Used by new TLS error page
+var L_NONDEFAULT_SETTINGS_TEXT = "Your TLS security settings aren\u2019t set to the defaults, which could also be causing this error.";
+
+//used by insecurefallbackblock.htm
+var L_InsecureSSL3FallbackDetected_TEXT = "Error: Connecting to this site has been blocked because it would require an insecure fallback to SSL 3.0";
+
+//used by fallbacknavcancel.htm
+var L_InsecureSSL3FallbackDetected_IFRAME_TEXT = "Error: A resource embedded in an iframe on this site has been blocked because it would require an insecure fallback to SSL 3.0";
+
+// Used by IM DNS error page
+var L_THIS_PAGE_CANT_BE_DISPLAYED_TEXT = "This page can\u2019t be displayed";
+var L_YOU_ARE_NOT_CONNECTED_TEXT = "You\u2019re not connected";
+
+// Used by IExtensionValidation error page
+var L_ActiveXUnsafe_TEXT = "%s blocked this website";
+var L_ActiveXUnsafe_Frame_TEXT = "%s blocked content on this website";
+var L_ActiveXUnsafe_Explain_TEXT = "%s blocked this site because it might contain threats to your PC or your privacy.";
+var L_ActiveXUnsafe_Hosted_TEXT = "Hosted by: %s";
