5f0003ab3dffaac4caf5ea772b9882610b6fb6f0
Aikoyori <12034280+Aikoyori@users.noreply.github.com>
2021-06-26 13:33:30 +0700

    Fix Proxy Shit I guess

---
+++ js/proxyshitfix.js
@@ -0,0 +1,15 @@
+function dotheredirect(relativeLinkTo)
+{
+    hostnameshit = window.location.origin
+    window.location = hostnameshit+relativeLinkTo
+}
+const $elems = document.querySelectorAll('a')
+var elems = Array.from($elems)
+elems.map(a => {
+  a.onclick = (e) => {
+    e.preventDefault()
+    console.log("REDIRECT SUCCESS?")
+
+    dotheredirect(a.getAttribute("href"))
+  }
+})
