3d57d1b8cc0ace60ae3c99961b464590aef32b2a
Aikoyori <12034280+Aikoyori@users.noreply.github.com>
2021-07-19 23:06:39 +0700

    rollback basically

--- js/proxyshitfix.js
+++ js/proxyshitfix.js
@@ -1,15 +1,17 @@
-function dotheredirect(relativeLinkTo)
-{
-    hostnameshit = window.location.origin
-    window.location = hostnameshit+relativeLinkTo
+function dotheredirect(relativeLinkTo) {
+  hostnameshit = window.location.origin
+  window.location = hostnameshit + relativeLinkTo
 }
-const $elems = document.querySelectorAll('a')
-var elems = Array.from($elems)
-elems.map(a => {
-  a.onclick = (e) => {
-    e.preventDefault()
-    console.log("REDIRECT SUCCESS?")
 
-    dotheredirect(a.getAttribute("href"))
-  }
-})
+function mapeverything() {
+  const $elems = document.querySelectorAll('a')
+  var elems = Array.from($elems)
+  elems.map(a => {
+    a.onclick = (e) => {
+      e.preventDefault()
+      console.log("REDIRECT SUCCESS?")
+
+      dotheredirect(a.getAttribute("href"))
+    }
+  })
+}
